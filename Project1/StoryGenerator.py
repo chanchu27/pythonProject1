0df3ed7431c2dd2d03b6b6f3fdcf906bf57fec7a
Chanchu27 <chanchalrajak219@gail.com>
2021-05-18 22:46:04 +0530

    Initial commit

---
+++ Project1/StoryGenerator.py
@@ -0,0 +1,12 @@
+import random
+when = ['A few years ago', 'Yesterday', 'Last night', 'A long time ago','On 20th Jan']
+who = ['a rabbit', 'an elephant', 'a mouse', 'a turtle','a cat']
+name = ['Arun', 'Mini','daisy', 'Hema', 'Stuart']
+residence = ['china','India', 'Germany', 'Poland', 'England']
+went = ['cinema', 'university','seminar', 'school', 'laundry']
+happened = ['made a lot of friends','Eats a burger', 'found a secret key',
+            'solved a mistery', 'wrote a book']
+print(random.choice(when) + ', ' + random.choice(who) + ' that lived in '
+      + random.choice(residence) +
+      ', went to the ' + random.choice(went) +
+      ' and ' + random.choice(happened))
